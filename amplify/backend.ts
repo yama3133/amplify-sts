862081f0218a1b396f961709694eee06ea60c11e
Yuki Yamashita <yamashita-y@example.com>
2026-05-12 09:58:31 +0900

    remove unused amplify backend files

--- amplify/backend.ts
+++
@@ -1,9 +0,0 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { sessionFunction } from "./functions/session/resource";
-
-export const backend = defineBackend({ sessionFunction });
-
-backend.sessionFunction.addEnvironment(
-  "OPENAI_API_KEY",
-  process.env.OPENAI_API_KEY ?? ""
-);
